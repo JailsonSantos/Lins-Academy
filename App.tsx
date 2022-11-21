ffb389bf339de26ee55e17e143f6215b4e46b91b
JailsonSantos <jailson.ads007@gmail.com>
2022-11-21 13:52:04 -0300

    Component Library - Imagem de Background

--- App.tsx
+++ App.tsx
@@ -4,6 +4,7 @@ import { Text, View, StatusBar } from 'react-native';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 import { Loading } from '@components/Loading';
 import { THEME } from './src/theme';
+import { SignIn } from '@screens/Signin';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -21,7 +22,9 @@ export default function App() {
         backgroundColor="transparent"
       />
 
-      <Loading />
+      {fontsLoaded ? <SignIn /> :
+        <Loading />
+      }
 
     </NativeBaseProvider>
 
