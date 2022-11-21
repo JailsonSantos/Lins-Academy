d7704930a0e1dec5b91beded0ceb1db9467e1066
JailsonSantos <jailson.ads007@gmail.com>
2022-11-21 14:05:00 -0300

    Component Library - Utilizando SVG

--- src/screens/Signin.tsx
+++ src/screens/Signin.tsx
@@ -1,5 +1,8 @@
-import { VStack, Image } from 'native-base'
+import { VStack, Image, Text, Center } from 'native-base'
+
+import LogoSvg from '@assets/logo.svg'
 import BackgroundImg from '@assets/background.png'
+
 export function SignIn() {
   return (
     <VStack flex={1} bg="gray.700">
@@ -10,6 +13,15 @@ export function SignIn() {
         resizeMode="contain"
         position="absolute"
       />
+
+      <Center my={24}>
+        <LogoSvg />
+
+
+        <Text color="gray.100" fontSize="sm">
+          Treine sua mente e o seu corpo
+        </Text>
+      </Center>
     </VStack>
   )
 }
