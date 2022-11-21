a7656b2bf30cfa10785fbd599249294413c510aa
JailsonSantos <jailson.ads007@gmail.com>
2022-11-21 16:53:53 -0300

    Rotas Privadas - Alterando os ícones da Bottom Navigator

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -5,6 +5,12 @@ import { History } from '@screens/History';
 import { Profile } from '@screens/Profile';
 import { Exercise } from '@screens/Exercise';
 
+import HomeSvg from '@assets/home.svg'
+import HistorySvg from '@assets/history.svg'
+import ProfileSvg from '@assets/profile.svg'
+import { useTheme } from 'native-base'
+
+
 type AppRoutes = {
   home: undefined;
   exercise: undefined;
@@ -17,12 +23,48 @@ export type AppNavigatorRouesProps = BottomTabNavigationProp<AppRoutes>
 const { Navigator, Screen } = createBottomTabNavigator<AppRoutes>();
 
 export function AppRoutes() {
+
+  const { sizes } = useTheme();
+  const iconSize = sizes[6] // tamanho dos icones
+
   return (
-    <Navigator>
-      <Screen name="home" component={Home} />
-      <Screen name="profile" component={Profile} />
-      <Screen name="history" component={History} />
-      <Screen name="exercise" component={Exercise} />
+    <Navigator screenOptions={{
+      headerShown: false,
+      tabBarShowLabel: false
+    }}>
+      <Screen
+        name="home"
+        component={Home}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <HomeSvg fill={color} width={iconSize} height={iconSize} />
+          )
+        }}
+      />
+      <Screen
+        name="history"
+        component={History}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <HistorySvg fill={color} width={iconSize} height={iconSize} />
+          )
+        }}
+      />
+      <Screen
+        name="profile"
+        component={Profile}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <ProfileSvg fill={color} width={iconSize} height={iconSize} />
+          )
+        }}
+      />
+
+      <Screen
+        name="exercise"
+        component={Exercise}
+
+      />
     </Navigator>
   )
 }
