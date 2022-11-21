1adc7d5b0498011ff5b3e17cc674bb029e15573d
JailsonSantos <jailson.ads007@gmail.com>
2022-11-21 13:41:39 -0300

    Component Library - Customizando o tema default

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ import { NativeBaseProvider } from 'native-base';
 import { Text, View, StatusBar } from 'react-native';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 import { Loading } from '@components/Loading';
+import { THEME } from './src/theme';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -12,7 +13,7 @@ export default function App() {
 
 
   return (
-    <NativeBaseProvider>
+    <NativeBaseProvider theme={THEME}>
 
       <StatusBar
         translucent
