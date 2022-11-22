f4b15b5e47dd634147d6048c6cf531cee96e5fcf
JailsonSantos <jailson.ads007@gmail.com>
2022-11-22 14:14:47 -0300

    Finalizando as Interfaces - Listando os grupos

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,31 +1,33 @@
 import { useState } from 'react'
 import { Group } from '@components/Group'
-import { HStack, VStack } from 'native-base'
+import { HStack, VStack, FlatList } from 'native-base'
 import { HomeHeader } from '@components/HomeHeader'
 
 export function Home() {
+  const [groups, setGroups] = useState(["Ombro", "Peito", "Costas", "Pernas"])
   const [groupSelected, setGroupSelected] = useState('Costas');
 
   return (
     <VStack flex={1}>
       <HomeHeader />
-      <HStack>
-        <Group
-          name="Ombro"
-          isActive={groupSelected === "Ombro"}
-          onPress={() => setGroupSelected("Ombro")}
-        />
-        <Group
-          name="Costas"
-          isActive={groupSelected === "Costas"}
-          onPress={() => setGroupSelected("Costas")}
-        />
-        <Group
-          name="Pernas"
-          isActive={groupSelected === "Pernas"}
-          onPress={() => setGroupSelected("Pernas")}
-        />
-      </HStack>
+
+      <FlatList
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <Group
+            name={item}
+            isActive={groupSelected === item}
+            onPress={() => setGroupSelected(item)}
+          />
+        )}
+        my={10}
+        maxH={10}
+        horizontal
+        _contentContainerStyle={{ px: 8 }}
+        showsHorizontalScrollIndicator={false}
+      />
+
     </VStack>
   )
 }
