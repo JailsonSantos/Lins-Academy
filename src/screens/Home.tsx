b30c1f5f4fe640799bef05b103b1d16ac5e423cc
JailsonSantos <jailson.ads007@gmail.com>
2022-11-22 14:49:26 -0300

    Finalizando as Interfaces - Listando exercícios

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,9 +1,11 @@
 import { useState } from 'react'
 import { Group } from '@components/Group'
-import { HStack, VStack, FlatList } from 'native-base'
+import { HStack, VStack, FlatList, Heading, Text } from 'native-base'
 import { HomeHeader } from '@components/HomeHeader'
+import { ExerciseCard } from '@components/ExerciseCard'
 
 export function Home() {
+  const [exercises, setExercises] = useState(["Remada alta", "Puxador Frontal", "Agachamento Lateral", "Puxador Costas", "Leg Press"])
   const [groups, setGroups] = useState(["Ombro", "Peito", "Costas", "Pernas"])
   const [groupSelected, setGroupSelected] = useState('Costas');
 
@@ -28,6 +30,27 @@ export function Home() {
         showsHorizontalScrollIndicator={false}
       />
 
+      <VStack flex={1} px={8}>
+        <HStack justifyContent="space-between" mb={5}>
+          <Heading color="gray.200" fontSize="md">
+            Exercicios
+          </Heading>
+          <Text color="gray.200" fontSize="sm">
+            4
+          </Text>
+        </HStack>
+
+        <FlatList
+          data={exercises}
+          keyExtractor={item => item}
+          renderItem={({ item }) => (
+            <ExerciseCard />
+          )}
+          showsVerticalScrollIndicator={false}
+          _contentContainerStyle={{ paddingBottom: 20 }}
+        />
+
+      </VStack>
     </VStack>
   )
 }
